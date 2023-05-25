36706c8ef37357b4866e096cff038423b9415814
Charahi <134573335+lov4love@users.noreply.github.com>
2023-05-24 21:43:56 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function() {
+    alert('Desde el corazón de sarahi, no se programar mucho amor, hice esto desde el fondo de mi corzon, quise hacerlo de una manera muy especial para ti, quiero que sepas que estoy muy feliz de estar a tu lado estos dos años, me eh dado cuenta que necesito mucho de ti, como tu necesitas de mi, me encanta mucho sentir todo tu cariño y amor que me expresas, jamás habia llegado amar a alguien como yo te amo a ti, quiero que recuerdes este día muy especial, ya que gracias a este dia hace dos añitos nos conocimos de manera muy divertida, amo que me acompañes en cada momento, eres mi mitad de mi vida, te aprecio, todo el amor del mundo junto no podría comprarse lo que yo siento por ti, porque mi amor no tiene números, ya que es un valor infinito, te amo demasiado, siempre recordare y estaré contigo, en cualquier momento y situacion que pase yo estaré contigo para siempre, feliz 2 años juntos Pablo te amo, att: sarahi<3')
+})
+
+
